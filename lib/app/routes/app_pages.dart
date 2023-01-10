aac0211a64f81f57f346f0f99fb7bf73f58bef66
Rahman <abdulrahman13081999@gmail.com.com>
2023-01-10 16:31:51 +0530

    home

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,41 @@
+import 'package:get/get.dart';
+
+import '../modules/cart/bindings/cart_binding.dart';
+import '../modules/cart/views/cart_view.dart';
+import '../modules/category/bindings/category_binding.dart';
+import '../modules/category/views/category_view.dart';
+import '../modules/details/bindings/details_binding.dart';
+import '../modules/details/views/details_view.dart';
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () =>  HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.CATEGORY,
+      page: () => const CategoryView(),
+      binding: CategoryBinding(),
+    ),
+    GetPage(
+      name: _Paths.CART,
+      page: () => const CartView(),
+      binding: CartBinding(),
+    ),
+    GetPage(
+      name: _Paths.DETAILS,
+      page: () => const DetailsView(),
+      binding: DetailsBinding(),
+    ),
+  ];
+}
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,18 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const CATEGORY = _Paths.CATEGORY;
+  static const CART = _Paths.CART;
+  static const DETAILS = _Paths.DETAILS;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const CATEGORY = '/category';
+  static const CART = '/cart';
+  static const DETAILS = '/details';
+}
